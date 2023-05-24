185450e34035c81d9ff6cde1913f503d4daa8a86
Wambui Mugo <mugomargaret128@gmail.com>
2023-05-24 18:54:41 +0300

    bubble sort and insertion sort solved and commented

--- Bubble sort.py
+++ Bubble sort.py
@@ -1,3 +1,50 @@
+# Function Description
+#
+# Complete the function countSwaps in the editor below.
+#
+# countSwaps has the following parameter(s):
+#
+# int a[n]: an array of integers to sort
+# Prints
+#
+# Print the three lines required, then return. No return value is expected.
+#
+# Input Format
+#
+# The first line contains an integer, , the size of the array .
+# The second line contains  space-separated integers .
+#
+# Constraints
+#
+# Output Format
+#
+# Sample Input 0
+#
+# STDIN   Function
+# -----   --------
+# 3       a[] size n = 3
+# 1 2 3   a = [1, 2, 3]
+# Sample Output 0
+#
+# Array is sorted in 0 swaps.
+# First Element: 1
+# Last Element: 3
+# Explanation 0
+# The array is already sorted, so  swaps take place.
+#
+# Sample Input 1
+#
+# 3
+# 3 2 1
+# Sample Output 1
+#
+# Array is sorted in 3 swaps.
+# First Element: 1
+# Last Element: 3
+# Explanation 1
+# The array is not sorted, and its initial values are: . The following  swaps take place:
+#
+# At this point the array is sorted and the three lines of output are printed to stdout.
 import math
 import os
 import random
--- Insertion sort.py
+++ Insertion sort.py
@@ -1,3 +1,44 @@
+# Function Description
+#
+# Complete the insertionSort1 function in the editor below.
+#
+# insertionSort1 has the following parameter(s):
+#
+# n: an integer, the size of
+# arr: an array of integers to sort
+# Returns
+#
+# None: Print the interim and final arrays, each on a new line. No return value is expected.
+# Input Format
+#
+# The first line contains the integer , the size of the array .
+# The next line contains  space-separated integers .
+#
+# Constraints
+#
+#
+#
+# Output Format
+#
+# Print the array as a row of space-separated integers each time there is a shift or insertion.
+#
+# Sample Input
+#
+# 5
+# 2 4 6 8 3
+# Sample Output
+#
+# 2 4 6 8 8
+# 2 4 6 6 8
+# 2 4 4 6 8
+# 2 3 4 6 8
+# Explanation
+#
+#  is removed from the end of the array.
+# In the st line , so  is shifted one cell to the right.
+# In the nd line , so  is shifted one cell to the right.
+# In the rd line , so  is shifted one cell to the right.
+# In the th line , so  is placed at position .
 #!/bin/python3
 
 import math
